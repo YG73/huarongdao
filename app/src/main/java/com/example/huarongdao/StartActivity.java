8619c00314df0e0385df66cd2f05b674d780870b
YG73 <xr.yu.life@outlook.com>
2025-05-29 23:58:16 +0800

    修改了readme

--- app/src/main/java/com/example/huarongdao/StartActivity.java
+++ app/src/main/java/com/example/huarongdao/StartActivity.java
@@ -12,7 +12,7 @@ import androidx.appcompat.app.AlertDialog;
 
 public class StartActivity extends Activity {
     private MediaPlayer bgmPlayer;
-    @Override
+    @Override 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
